b6b09b0de3ab656a32720fb44ad44df8e86819b6
benjaminparentt <p.benjamin620@icloud.com>
2024-04-12 16:55:46 +0200

    add 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""comment"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page with a list of all states."""
+    states = storage.all("State").values()
+    states = sorted(states, key=lambda state: state.name)  # Sort states by name
+    return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def close_session(exception):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
